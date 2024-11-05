b3402d35d73fd7620aea6a9dbfcc70812724aa8f
bmoclay <benmorgancleveland@gmail.com>
2024-11-05 00:32:20 -0500

    add new pages

---
+++ src/components/NewsContainer.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import News from "../components/News.jsx";
+
+
+
+const NewsContainer = () => {
+    
+    const [openModal, setOpenModal] = useState(false);
+
+     const handleCloseModal = () => {
+        setOpenModal(false);
+    };
+
+    const handleButtonClick = () => {
+        setOpenModal(true);
+      };
+
+    return (
+        <div className="pl-8 bg-white bg-opacity-95">
+            <div className="NewsContainer">
+                {!openModal && (
+                    <button onClick={handleButtonClick}>News</button>
+                )}
+                {openModal && <News closeModal={handleCloseModal}/>}
+            </div>
+        </div>
+    
+  )
+}
+
+export default NewsContainer
---
+++ src/data/bookData.js
@@ -0,0 +1,198 @@
+
+export const BOOKS = [
+     {
+        id: 1,
+        bookTitle: "",
+        headerArtist: "",
+        artist: "Vaclav Pozarek",
+        urlText: "first-book",
+        year: 2023,
+        pages: 12,
+        size: "7.5 x 10.5 cm",
+        binding: "staplebound",
+        price: "EU 30.-",
+        edition: "First",
+        editionAmount: 220,
+        attribution:
+          "",
+        notes:
+          "",
+        bio: "",
+        websiteUrl: "https://editionsbierisusanne-ebs.ch/",
+        website: "editionsbierisusanne-ebs.ch",
+        logo: logo1,
+        // coverImage: dog,
+        sliderStyles: "none",
+        images: [
+          {
+            id: 1,
+            imgFile: dog
+          },
+          {
+            id: 2,
+            imgFile: logoA2
+          },
+          {
+            id: 3,
+            imgFile: logoA3
+          },
+          {
+            id: 4,
+            imgFile: logoA4
+          },
+          {
+            id: 5,
+            imgFile: logoA5
+          },
+          {
+            id: 6,
+            imgFile: logoA6
+          },
+          {
+            id: 7,
+            imgFile: logoA7
+          },
+          {
+            id: 8,
+            imgFile: logoA8
+          },
+          {
+            id: 9,
+            imgFile: logoA9
+          },
+          {
+            id: 10,
+            imgFile: logoA10
+          },
+          {
+            id: 11,
+            imgFile: logoA11
+          },
+          {
+            id: 12,
+            imgFile: logoA12
+          },
+          {
+            id: 13,
+            imgFile: logoA13
+          },
+        ],
+        available: true,
+      },
+    {
+        id: 2,
+        bookTitle: "LAWS, MATTER",
+        headerArtist: "Isabelle Cornaro",
+        artist: "Isabelle Cornaro",
+        urlText: "isabelle-cornaro",
+        year: 2024,
+        pages: 44,
+        size: "24 x 30 cm",
+        binding: "Staplebound",
+        price: "EU 30.-",
+        edition: "First",
+        editionAmount: 220,
+        attribution:  "No text or explanation, so-called BAT - reproduced and overworked reproductions - combine to form a cinematic outline through spray paintings and their multiple enlargements into infinity.",          
+        notes: "Published on occasion of the exhibition MOTHER, LAWS, MATTER, from March 21 to June 29, 2024, at Fondazione Giuliani, Roma by e.bs editionsbierisusanne CH-3012 Bern (main office) and IT-06031 Bevagna (PG) (additional office/non-profit project space Via Santa Maria 7) 2024. Realized by La Tipografica Bevagna.",
+        bio: "Isabelle CORNARO - born 1974, France. Lives and works in Paris and Geneva - her practice includes video, installation, sculpture, painting, scenography as well as edition and artist's books...; she explores the relationship between the object and its image, the original and its copy and the interface between art and art history.",
+        websiteUrl: "https://isabellecornaro.com/",
+        website: "isabellecornaro.com",
+        logo: logo2,
+        images: [
+          {
+            id: 1,
+            imgFile: coverImage1
+          }, 
+          {
+            id: 2,
+            imgFile: coverImage2
+          }, 
+          {
+            id: 3,
+            imgFile: coverImage3
+          },  
+          {
+            id: 4,
+            imgFile: isaCorWOP1
+          },
+          {
+            id: 5,
+            imgFile: isaCorWOP2
+          },
+          {
+            id: 6,
+            imgFile: isaCorWOP3
+          },  
+          {
+            id: 7,
+            imgFile: isaCorWOP4
+          },  
+          {
+            id: 8,
+            imgFile: isaCorWOP5
+          },  
+          {
+            id: 9,
+            imgFile: isaCorWOP6
+          },  
+          {
+            id: 10,
+            imgFile: isaCorWOP7
+          },  
+          {
+            id: 11,
+            imgFile: isaCorWOP8
+          },  
+          {
+            id: 12,
+            imgFile: isaCorWOP9
+          },  
+          {
+            id: 13,
+            imgFile: isaCorWOP10
+          },  
+          {
+            id: 14,
+            imgFile: isaCorWOP11
+          },  
+          {
+            id: 15,
+            imgFile: isaCorWOP12
+          },  
+          {
+            id: 16,
+            imgFile: isaCorWOP13
+          },  
+          {
+            id: 17,
+            imgFile: isaCorWOP14
+          },  
+          {
+            id: 18,
+            imgFile: isaCorWOP15
+          },  
+          {
+            id: 19,
+            imgFile: isaCorWOP16
+          },  
+          {
+            id: 20,
+            imgFile: isaCorWOP17
+          },  
+          {
+            id: 21,
+            imgFile: isaCorWOP18
+          },  
+          {
+            id: 22,
+            imgFile: isaCorWOP19
+          },  
+          {
+            id: 23,
+            imgFile: isaCorWOP20
+          }
+        ],
+        available: true,
+      },
+   
+]
